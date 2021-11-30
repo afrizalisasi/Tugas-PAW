64da7bb2869aef8b1d6eeeea54670d2c5bd3e2e3
afrizalisasi <rizal.reitan@gmail.com>
2021-11-30 21:56:52 +0700

    Added a Procfile.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const employeeController = require('./controllers/emplyeeController');
 //require(__dirname + '/models/db.js');
-require('./models/db')
 var app = express();
 app.use(bodyparser.urlencoded({
     extended: true
